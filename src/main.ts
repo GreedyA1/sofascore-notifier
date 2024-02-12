a52950549180fdfdf6877f02b9c4de408386f0d0
GreedyAi <m.vato122@gmail.com>
2024-02-13 01:44:21 +0400

    code attempt

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import axios from 'axios'
+import { Root,Event } from './types';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -12,3 +14,21 @@ app.get('/', (req, res) => {
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
+
+
+setInterval(() => {
+  axios.get<Root>('https://api.sofascore.com/api/v1/sport/football/events/live')
+    .then(response => {
+      const mappedEvents: Event[] = []
+      response.data.events.filter(event => event.hasXg).forEach(event => {
+        mappedEvents.push(event)
+      })
+      
+      // Process the data as needed
+    })
+    .then()
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      // Handle error
+    });
+}, 5000); // 5000 milliseconds = 5 seconds
